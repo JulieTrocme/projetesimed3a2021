9f97d2833f292157f0aee4756c8b94da77dd8c91
JulieTrocme <juju19952006@gmail.com>
2022-03-04 14:32:34 +0100

    Front terminé

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProduitController extends AbstractController
+{
+    /**
+     * @Route("/produit", name="produit")
+     */
+    public function produit()
+    {
+        return $this->render('front/default/produit.html.twig', [
+
+        ]);
+    }
+
+
+}
